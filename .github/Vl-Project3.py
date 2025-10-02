eafbd677d9d6c1915a20b3b10515a1a726bdb0d4
vlatson647-hash <vlatson647@phm.k12.in.us>
2025-10-02 09:47:52 -0400

    Update Vl-Project3.py

--- .github/Vl-Project3.py
+++ .github/Vl-Project3.py
@@ -7,7 +7,7 @@ import random
 amount = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 most_Favorite = ["McDonalds", "Burger King", "Wendy's", "Arby's", "Chick fil-a", "Others", "KFC", "Culver's", "Popeye's"]
 least_Favorite = ["McDonalds", "Burger King", "Wendy's", "Arby's", "Chick fil-a", "Others", "KFC", "Culver's", "Popeye's"]
-calories = ["<1000", 1000-2000, 2000-3000, 3000-4000, 4000-5000, ">5000"]
+calories = ["<1000", 1000, 2000, 2000, 3000, 3000, 4000, 4000, 5000, ">5000"]
 
 
 data = {
@@ -15,11 +15,11 @@ data = {
     "Times Ate (MF)": [random.randint(0,10)for _ in amount],
     "Least Favorite": [random.choice(least_Favorite) for _ in amount],
     "Times Ate (LF)": [random.randint(0,10) for _ in amount],
-    "Calories": [random.choice(calories) for _ in amount],
+    "Calories per meal": [random.choice(calories) for _ in amount],
 }
 
 surveyData = pd.DataFrame(data)
-
+print(surveyData)
 surveyData.to_csv("Survey.csv", index=False)
 
 
